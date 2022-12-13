75c6797f1ccc174ab2e8715324345b0bc9236d85
Vu Anh Huy <huy.vu@bluecoglobal.com>
2022-12-13 21:54:43 +0700

    init

--- src/main/java/com/tass/productservice/security/WebSecurity.java
+++ src/main/java/com/tass/productservice/security/WebSecurity.java
@@ -22,6 +22,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     private UserDetailsService userDetailsService;
 
 
+
+
     public WebSecurity(UserDetailServiceImpl userDetailsServiceImpl) {
         passwordEncoder = new BCryptPasswordEncoder();
         userDetailsService = userDetailsServiceImpl;
